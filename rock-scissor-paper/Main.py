61cf7150a9a9d9c71cd2a1406ef74f4201ff9add
Abdulkayyum Uçkun <abdulkayum.uckun@gmail.com>
2017-01-11 12:20:02 +0200

    3rd commit to the python file

--- rock-scissor-paper/Main.py
+++ rock-scissor-paper/Main.py
@@ -1,4 +1,8 @@
+from random import randint
+
 objects = ('Rock', 'Paper', 'Scissor')
+human_object = ''
+ai_object = ''
 
 
 def list_objects():
@@ -20,8 +24,61 @@ def process_user_choice(user_input):
         user_object = objects[1]
     elif user_input == 's':
         user_object = objects[2]
-    return user_object
+    else:
+        print('Invalid input, try again')
+        ask_user_input()
+
+    if user_object in objects:
+        global human_object
+        human_object = user_object
+        print('Your choice was: ', human_object)
 
 
-list_objects()
-ask_user_input()
+def ai_choice():
+    ai_input = objects[randint(0, 2)]
+    print('Computer choice was: ', ai_input)
+    global ai_object
+    ai_object = ai_input
+
+
+def compare_inputs():
+    if human_object == 'Rock':
+        if ai_object == 'Rock':
+            print('TIE!')
+        elif ai_object == 'Scissor':
+            print('HUMAN WIN!')
+        elif ai_object == 'Paper':
+            print('AI WİN!!')
+    elif human_object == 'Scissor':
+        if ai_object == 'Rock':
+            print('AI WİN!!')
+        elif ai_object == 'Scissor':
+            print('TIE!')
+        elif ai_object == 'Paper':
+            print('HUMAN WIN!')
+    elif human_object == 'Paper':
+        if ai_object == 'Rock':
+            print('HUMAN WIN!')
+        elif ai_object == 'Scissor':
+            print('AI WİN!!')
+        elif ai_object == 'Paper':
+            print('TIE!')
+
+
+def start_new_turn():
+    print('type \'y\' for new turn or \'n\' to terminate: ')
+    input_user = input(':')
+    if input_user == 'y':
+        process_game()
+    elif input_user == 'n':
+        return
+
+
+def process_game():
+    list_objects()
+    ask_user_input()
+    ai_choice()
+    compare_inputs()
+    start_new_turn()
+
+process_game()
